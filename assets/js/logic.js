5c73c50e31c714816f34b97cdd25da381f1f4215
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-18 22:00:13 +0000

    change css variables to implement theme toggling

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,12 @@
 function changeTheme(event) {
-    console.log($(":root").css('--mov_color'));
-    console.log($(':root').style.getPropertyValue('--mov_color'));
-    if ($(':root').css('--mov_color') == 'rgb(99, 99, 236)') $(':root').css('--mov_color');
-    else $(':root').attr('--mov_color', 'rgb(99, 99, 236)')
+    let root = $(':root');
+    if (root.css('--mov_color') == 'rgb(99, 99, 236)') {
+         root.css('--mov_color', 'rgb(123, 123, 223)');
+         root.css('--bg-color', 'rgb(40, 40, 74)');
+         root.css(' --txt-color', 'rgb(255, 255, 255)');
+         root.css('--task-color', 'rgb(51, 51, 92)');
+         root.css('--sub-task-color', 'rgba(255, 255, 255, 0.41)');
+    }
 }
 
 $('.fa-toggle-on').click(changeTheme);
