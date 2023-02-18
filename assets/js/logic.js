f392f04dbf10a6dfd2737b82d29530c75d44e520
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-18 22:14:52 +0000

    make theme toggling possible

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,9 +3,18 @@ function changeTheme(event) {
     if (root.css('--mov_color') == 'rgb(99, 99, 236)') {
          root.css('--mov_color', 'rgb(123, 123, 223)');
          root.css('--bg-color', 'rgb(40, 40, 74)');
-         root.css(' --txt-color', 'rgb(255, 255, 255)');
+         root.css('--txt-color', 'rgb(255, 255, 255)');
          root.css('--task-color', 'rgb(51, 51, 92)');
          root.css('--sub-task-color', 'rgba(255, 255, 255, 0.41)');
+         root.css('--box-shadow', '2px 2px 2px rgba(0, 0, 0, 0.387)');
+    }
+    else {
+        root.css('--mov_color', 'rgb(99, 99, 236)');
+        root.css('--bg-color', 'rgb(227, 247, 246)');
+        root.css('--txt-color', 'black');
+        root.css('--task-color', 'white');
+        root.css('--sub-task-color', 'rgba(0, 0, 0, 0.387)');
+        root.css('--box-shadow', '3px 3px 2px #aaaaaaa0');
     }
 }
 
