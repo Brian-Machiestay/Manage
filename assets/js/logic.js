39a0deb02b1e1942c58aa0dd95e011f580f728a1
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-20 17:54:05 +0000

    complete view task design mvp

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,7 +10,7 @@ function changeTheme(event) {
          root.css('--sub-task-color', 'rgba(255, 255, 255, 0.41)');
          root.css('--box-shadow', '2px 2px 2px rgba(0, 0, 0, 0.387)');
          root.css('--sidebar-border', '1px solid rgba(242, 237, 237, 0.12)');
-         toggle.addClass('fa-toggle-off').removeClass('fa-toggle-on');
+         toggle.addClass('fa-toggle-on').removeClass('fa-toggle-off');
     }
     else {
         root.css('--mov_color', 'rgb(99, 99, 236)');
@@ -20,7 +20,7 @@ function changeTheme(event) {
         root.css('--sub-task-color', 'rgba(0, 0, 0, 0.387)');
         root.css('--box-shadow', '3px 3px 2px #aaaaaaa0');
         root.css('--sidebar-border', '1px solid rgba(242, 237, 237, 0.571)');
-        toggle.addClass('fa-toggle-on').removeClass('fa-toggle-off');
+        toggle.addClass('fa-toggle-off').removeClass('fa-toggle-on');
     }
 }
 
