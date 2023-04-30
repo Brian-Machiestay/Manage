e8f1e76ed1e59c458d48943fc005d812f42bcad9
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-30 01:31:53 +0000

    check template paths and remove unnecessary routes

--- flask-web/auth/view.py
+++ flask-web/auth/view.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """define an auth view for this application"""
 
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 
 auth_blueprint = Blueprint('auth', __name__)
 
 @auth_blueprint.route('/login')
 def login():
-    return 'Login'
+    return(render_template('auth/login.html'))
 
 @auth_blueprint.route('/signup')
 def signup():
-    return 'Signup'
+    return(render_template('auth/signup.html'))
 
 @auth_blueprint.route('/logout')
 def logout():
